1ae58ebc6b70f691589897c7420d8cf12527a70f
EC2 Default User <ec2-user@ip-172-31-33-43.us-east-2.compute.internal>
2020-12-31 01:15:02 +0000

    Initial commit

---
+++ run_aws_exercise.sh
@@ -0,0 +1,9 @@
+
+
+export AWS_ACCESS_KEY_ID='AKIAVRTKZCMUYYAUIH4Z'
+export AWS_SECRET_ACCESS_KEY='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'
+
+#export ANSIBLE_HOST_KEY_CHECKING='False'
+#export PRIVATE_KEY_FILE='devops_aws_exercise.pem'
+
+
