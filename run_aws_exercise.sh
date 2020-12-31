912e10164e867e94fc5523589a069bfabe75e66e
jasontaylor0891 <71782859+jasontaylor0891@users.noreply.github.com>
2020-12-31 09:37:11 -0500

    Update run_aws_exercise.sh

--- run_aws_exercise.sh
+++ run_aws_exercise.sh
@@ -1,9 +1,63 @@
 
+#!/bin/bash
+#########################################################################################################
+##
+##  SCRIPT:            run_aws_exercise.sh
+## 
+##  AUTHORS:           Jason Taylor 
+##                       
+##  CREATION DATE:     12/30/2020
+##
+##  REQUIREMENTS:      None 
+##
+##  DEPENDENCIES:      None
+## 
+##  DESCRIPTION:       This script is used to run the EPM Cloud createService on the OCI environment.
+##
+##  EXAMPLE USAGE:     ./run_aws_exercise.sh aws_access_key_id aws_secret_access_key instance_size instance_name file
+##          
+#########################################################################################################
+#----------------------------------
+# SCRIPT HEADER
+#----------------------------------
 
-export AWS_ACCESS_KEY_ID='AKIAVRTKZCMUYYAUIH4Z'
-export AWS_SECRET_ACCESS_KEY='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'
+#----------------------------------
+# GLOBAL VARIABLES
+#----------------------------------
 
-#export ANSIBLE_HOST_KEY_CHECKING='False'
-#export PRIVATE_KEY_FILE='devops_aws_exercise.pem'
 
+#----------------------------------
+# FUNCTIONS
+#----------------------------------
+# -------------------------------------------------------------
+# Name       : usage
+# Displays usage information for the script              
+# Parameters : None                           
+# Returns    : None
+# -------------------------------------------------------------
+function usage
+{
+    echo "Usage: ./run_aws_exercise.sh aws_access_key_id aws_secret_access_key instance_size instance_name file"
+}
+
+
+#----------------------------------
+# MAIN PROGRAM
+#----------------------------------
+function main
+{
+
+    if (( $# != 5 )); then
+        usage
+        exit 1
+    fi
+    
+    demofilesloc="/unit/work"
+    resourceName=$2
+    printf "INFO: Create Service was successful.\n"
+	
+    exit 0
+}
+
+main "$@"
 
